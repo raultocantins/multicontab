14de683cfbedab34fed62ee5bcc4d35496961e45
Alex Raul <alexbraul.ar@gmail.com>
2024-03-26 22:09:15 -0300

    adição de uma nova feature de kanban(experimental) e alterações de layout

--- backend/src/controllers/KanbanController.ts
+++ backend/src/controllers/KanbanController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import KanbanService from "../services/KanbanServices/KanbanService";
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
-  const { tickets, users } = await KanbanService({});
+  const { tickets, users } = await KanbanService();
 
   return res.status(200).json({ tickets, users });
 };
